0b72d71b9ebd21f8c3b56d708cc9b5b4e65292b4
Maris Orbidans <maris.orbidans@onthemarket.com>
2018-10-26 02:27:33 +0100

    int32

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,6 @@
 module Main where
 
-import Data.Text (Text, split)
-import qualified Data.Text.IO as TIO
-import qualified Data.Text.Read as R
-
 import Lib (lowestIntegerNotInList)
 
-parseText :: Text -> Int
-parseText text =
-  case R.signed R.decimal text of
-    Left e -> error e
-    Right (i, _) -> i
-
-readInput :: IO [Int]
-readInput = do
-  line <- TIO.getLine
-  return $ map parseText $ split (== ',') line
-
 main :: IO ()
-main = do
-  st <- readInput
-  print $ lowestIntegerNotInList st
+main = lowestIntegerNotInList >>= print
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,32 +1,62 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Lib (lowestIntegerNotInList) where
 
+import Data.Int
 import Data.Array.Unboxed
-import Data.Array.ST (STUArray, newListArray, runSTUArray, readArray, writeArray)
-import Data.Array.Base (MArray)
+import Data.Array.ST (STUArray, readArray, writeArray)
+import Data.Array.IO (IOUArray)
 import Control.Monad (when, forM_)
+import Data.Array.MArray (newArray)
 
-replace :: (MArray a e m, Ix e, Num e) => a e e -> e -> e -> m ()
+import Data.Text (Text)
+import qualified Data.Text.IO as TIO
+import qualified Data.Text.Read as R
+
+signed :: Text -> Int32
+signed text =
+  case R.signed R.decimal text of
+    Left e -> error e
+    Right (i, _) -> i
+
+decimal :: Text -> Int
+decimal text =
+  case R.decimal text of
+    Left e -> error e
+    Right (i, _) -> i
+
+readInput :: IO Text
+readInput = TIO.getLine
+
+createNewArray :: Int -> IO (IOUArray Int Int32)
+createNewArray size = newArray (1, size) 0
+
+replace :: IOUArray Int Int32 -> Int -> Int -> IO ()
 replace stArr idx size =
   when (idx > 0 && idx <= size) $ do
     val <- readArray stArr idx
-    when (val /= idx) $ do
-      writeArray stArr idx idx
-      replace stArr val size
+    when (val /= fromIntegral idx) $ do
+      writeArray stArr idx $ fromIntegral idx
+      replace stArr (fromIntegral val) size
 
-missingInt' :: Int -> Int -> UArray Int Int -> Int
-missingInt' i s xs
-  | i <= s = if xs ! i /= i then i else missingInt' (i + 1) s xs
-  | otherwise = 1 + s
+missingInt :: Int -> Int -> IOUArray Int Int32 -> IO Int32
+missingInt i s xs =
+  if i <= s then do
+    val <- readArray xs i
+    if val /= fromIntegral i then return $ fromIntegral i else missingInt (i + 1) s xs
+    else return $ fromIntegral $ 1 + s
 
-missingInt :: UArray Int Int -> Int
-missingInt xs = let (i,s) = bounds xs in missingInt' i s xs
+lowestIntegerNotInList :: IO Int32
+lowestIntegerNotInList = do
+  size <- decimal <$> readInput
+  stArr <- createNewArray size
 
-lowestIntegerNotInList :: [Int] -> Int
-lowestIntegerNotInList xs = missingInt $ runSTUArray $ do
-  let size = length xs
-  stArr <- newListArray (1, size) xs
-  forM_ [1 .. size] $ \i -> do
-    val <- readArray stArr i
-    when (val /= i) $
-      replace stArr val size
-  return stArr
+  forM_ [1 .. size] $ \idx -> do
+    i <- signed <$> readInput
+    writeArray stArr idx i
+
+  forM_ [1 .. size] $ \idx -> do
+    val <- readArray stArr idx
+    when (val /= fromIntegral idx) $
+      replace stArr (fromIntegral val) size
+
+  missingInt 1 size stArr
